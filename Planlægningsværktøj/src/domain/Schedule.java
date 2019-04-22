17bf3fdc119b49e69856b33b1cb9556337c7dab3
Timmy M. C <zlyzart@outlook.com>
2019-04-22 10:28:39 +0200

    cleanup

--- Planlægningsværktøj/src/domain/Schedule.java
+++ Planlægningsværktøj/src/domain/Schedule.java
@@ -1,30 +1,26 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class Schedule {
+
     //TODO update sequence diagram for the usecase opretAktivitet
     private ArrayList<Activity>[] schedule;
-    
-    
-    public Schedule(){
+
+    public Schedule() {
         schedule = new ArrayList[7];
         for (int i = 0; i < 7; i++) {
-        schedule[i] = new ArrayList<>();    
+            schedule[i] = new ArrayList<>();
         }
-        
-        
+
     }
 
     public ArrayList<Activity>[] getSchedule() {
         return schedule;
     }
 
-    
     public void addActivity(Activity activity, int day) {
         schedule[day].add(activity);
-        
+
     }
 }
