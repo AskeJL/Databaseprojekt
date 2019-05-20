159c59918f17bafa4a5da3b4593d11c224c9cb8e
Timmy M. C <zlyzart@outlook.com>
2019-05-20 11:07:27 +0200

    update tests

--- Planlægningsværktøj/test/database/DBControllerTest.java
+++ Planlægningsværktøj/test/database/DBControllerTest.java
@@ -3,30 +3,32 @@ package database;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class DBControllerTest {
-    
+
     DBController dbController;
     int authenticated = 0;
-    
+
     public DBControllerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         dbController = new DBController();
+        
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -38,12 +40,11 @@ public class DBControllerTest {
     @org.junit.Test
     public void testAuthenticate() {
         authenticated = dbController.authenticate("sosu_test", "sosutest");
-        Assert.assertEquals(authenticated,1);
+        assertEquals(1, authenticated);
         authenticated = dbController.authenticate("test1", "test1");
-        Assert.assertEquals(authenticated,2);
+        assertEquals(2, authenticated);
         authenticated = dbController.authenticate("wrongUsername", "wrongPassword");
         Assert.assertNotSame(authenticated, 1);
         Assert.assertNotSame(authenticated, 2);
     }
 }
-   
--- Planlægningsværktøj/test/domain/ControllerTest.java
+++ Planlægningsværktøj/test/domain/ControllerTest.java
@@ -1,18 +1,14 @@
 package domain;
 
-import com.sun.deploy.security.CredentialInfo;
 import domain.users.Citizen;
-import java.text.SimpleDateFormat;
 import java.sql.Date;
 import java.util.UUID;
-import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.postgresql.core.Oid;
 
 public class ControllerTest {
 
@@ -43,9 +39,9 @@ public class ControllerTest {
     public void setCurrentCitizenTest() {
         citizen = new Citizen("test1", "test1", "12345", Date.valueOf("2019-05-10"), UUID.fromString("50586125-bfc9-4e3d-920c-95fa7bc433e5"), new Schedule());
         controller.setCurrentCitizen(citizen.getId(), citizen.getUsername());
-        Assert.assertEquals(citizen.getUsername(), controller.getCurrentCitizen().getUsername());
-        Assert.assertEquals(citizen.getBirthday(), controller.getCurrentCitizen().getBirthday());
-        Assert.assertEquals(citizen.getId(), controller.getCurrentCitizen().getId());
-        Assert.assertEquals(citizen.getName(), controller.getCurrentCitizen().getName());
+        assertEquals(citizen.getUsername(), controller.getCurrentCitizen().getUsername());
+        assertEquals(citizen.getBirthday(), controller.getCurrentCitizen().getBirthday());
+        assertEquals(citizen.getId(), controller.getCurrentCitizen().getId());
+        assertEquals(citizen.getName(), controller.getCurrentCitizen().getName());
     }
 }
